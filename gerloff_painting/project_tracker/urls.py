fb3a38881a3f43a29c39a0ad82fe46c397c5633f
Lisa Taroli <TaroliFamily@Lisas-MacBook-Pro.local>
2020-03-09 22:32:17 -0400

    stubbed out all navigation, added pages to templates, urls, and views

--- gerloff_painting/project_tracker/urls.py
+++ gerloff_painting/project_tracker/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 app_name = 'project_tracker'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('job_list', views.JobListView.as_view(), name='jobs_list'),
+    path('open_items', views.OpenItemsView.as_view(), name='open_items'),
+    path('project_schedule', views.ProjectScheduleView.as_view(), name='project_schedule'),
+    path('inventory_control', views.InventoryControlView.as_view(), name='inventory_control'),
+    path('wallcovering', views.WallcoveringView.as_view(), name='wallcovering'),
+    path('go_to_job_finder', views.GoToJobFinderView.as_view(), name='go_to_job_finder'),
+    path('book_new_job', views.BookNewJobView.as_view(), name='book_new_job')
 ]
--- gerloff_painting/project_tracker/views.py
+++ gerloff_painting/project_tracker/views.py
@@ -18,6 +18,41 @@ django.setup()
 
 class IndexView(generic.ListView):
     template_name = 'project_tracker/index.html'
+    def get_queryset(self):
+        return;
+
+class JobListView(generic.ListView):
+    template_name = 'project_tracker/job_list.html'
     context_object_name = 'jobs'
     def get_queryset(self):
         return Jobs.objects.all().order_by('-booked_date')[0:2000]
+
+class OpenItemsView(generic.ListView):
+    template_name = 'project_tracker/open_items.html'
+    def get_queryset(self):
+        return;
+
+class ProjectScheduleView(generic.ListView):
+    template_name = 'project_tracker/project_schedule.html'
+    def get_queryset(self):
+        return;
+
+class InventoryControlView(generic.ListView):
+    template_name = 'project_tracker/inventory_control.html'
+    def get_queryset(self):
+        return;
+
+class WallcoveringView(generic.ListView):
+    template_name = 'project_tracker/wallcovering.html'
+    def get_queryset(self):
+        return;
+
+class GoToJobFinderView(generic.ListView):
+    template_name = 'project_tracker/go_to_job_finder.html'
+    def get_queryset(self):
+        return;
+
+class BookNewJobView(generic.ListView):
+    template_name = 'project_tracker/book_new_job.html'
+    def get_queryset(self):
+        return;
